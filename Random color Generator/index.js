34372c85d6b7610bdb8a9b58cb4cd08c0f0d1cdb
Shivam-Batham <117800149+Shivam-Batham@users.noreply.github.com>
2023-04-16 00:26:49 +0530

    Add files via upload

---
+++ Random color Generator/index.js
@@ -0,0 +1,27 @@
+const containercolor = ()=>{
+    const color = Math.floor(Math.random()*16777215+1);
+    const textcolor = "#" + color.toString(16);
+    document.getElementById("c").style.backgroundColor=textcolor;
+    //
+    let boxShadowcolor=color;
+    document.getElementById("c").style.boxShadow='5px 5px 10px white';
+    console.log(textcolor);
+}
+
+const getcolor = ()=>{
+    const randomcolor = Math.floor(Math.random()*16777215);
+    const code = "#" + randomcolor.toString(16);
+    // console.log(randomcolor,code);
+    document.body.style.background=code;
+    document.getElementById("text").innerText=code;
+    document.getElementById("btn").style.color=code;
+    document.getElementById("text").style.color=code;
+    
+    //
+    containercolor();
+}
+document.getElementById("btn").addEventListener("click",getcolor);
+getcolor();
+
+
+
